063284e26c0fb9cbd33bc51e9c8943626dba28a6
cesar <cesarluilly.garcia@hotmail.com>
2022-05-15 19:33:26 -0500

    add form

--- src/views/Formulario.vue
+++ src/views/Formulario.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="paddingTopHeightHeader">
     <div class="formulario">
-      <h1>Formulario para que las personas conoscan su contribución</h1>
+      <div class="container">
+        <h1 class="mb-2">Formulario para que las personas conoscan su contribución</h1>
+        <hr>
+        <h4 class="mb-5">¿Que tipo de residuo son los que desechan y en que promedio medido en Kilogramos?</h4>
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <form>
+              <div class="mb-3">
+                <label for="input1" class="form-label">Email address</label>
+                <input type="number" class="form-control" id="input1">
+                <div id="input1" class="form-text"></div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+              </div>
+              
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form> 
+
+          </div>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </template>
